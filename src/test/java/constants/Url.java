74571a4860104d1caba8d1446097a3b35120d440
Roberto Filho <roberto.siilva@hotmail.com>
2023-04-07 11:45:08 -0300

    Ajuste url base

--- src/test/java/constants/Url.java
+++ src/test/java/constants/Url.java
@@ -2,5 +2,5 @@ package constants;
 
 public class Url {
 
-    public static String MAIN_URL =  "https://www.grocerycrud.com/v1.x/demo/my_boss_is_in_a_hurry/bootstrap";
+    public static String MAIN_URL =  "https://www.petz.com.br";
 }
